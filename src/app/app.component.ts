501108c5f9178ea61eec2df5c0b5dca428a76617
yuber28G <garayladino2728@gmail.com>
2025-06-24 00:47:17 -0500

    Corrige navegación del menú y actualiza AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent { }
